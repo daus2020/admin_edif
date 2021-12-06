3a3331138f5a46c6dee055bb87c18778278bdd1a
daniel <danielandresus@gmail.com>
2021-12-06 15:49:12 -0300

    building eye edit icon working destroy only letters

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,5 +1,6 @@
 class Building < ApplicationRecord
-  has_many :apartments
+  has_many :apartments, dependent: :delete_all
+  # has_many :apartments
   belongs_to :user
 
   validates :name, :address, :city, presence: true
